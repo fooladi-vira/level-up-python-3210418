470a890ae3230bd5eb0dd0caafbce0aff26566da
zeinab fooladi <76722031+fooladi-vira@users.noreply.github.com>
2024-04-05 22:04:34 +0330

    Update diceware.py with me

--- src/11 Generate a Password/diceware.py
+++ src/11 Generate a Password/diceware.py
@@ -1,15 +1,26 @@
-import secrets
+import random
 
-def generate_passphrase(num_words, wordlist_path='diceware.wordlist.asc'):
-    with open(wordlist_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()[2:7778]
-        word_list = [line.split()[1] for line in lines]
+def load_diceware_wordlist(filename):
+    wordlist = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            parts = line.strip().split()
+            if len(parts) >= 2:  # Make sure the line has at least two parts
+                wordlist.append(parts[1])  # Assuming the word is the second part
+    return wordlist
 
-    words = [secrets.choice(word_list) for i in range(num_words)]
-    return ' '.join(words)
 
 
-# commands used in solution video for reference
-if __name__ == '__main__':
-    print(generate_passphrase(7))
-    print(generate_passphrase(7))
+def generate_passphrase(num_words, wordlist):
+    # Generate a passphrase by selecting random words from the wordlist
+    passphrase = ' '.join([random.choice(wordlist) for _ in range(num_words)])
+    return passphrase
+
+# Load the Diceware word list
+filename = 'diceware.wordlist.asc'
+wordlist = load_diceware_wordlist(filename)
+
+# Generate a passphrase
+num_words = 5  # Choose the number of words for the passphrase
+passphrase = generate_passphrase(num_words, wordlist)
+print(passphrase)
