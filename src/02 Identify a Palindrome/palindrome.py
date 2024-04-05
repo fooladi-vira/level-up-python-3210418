4bab40695c1efd82f18b4f797029ec152a8a8dc6
zeinab fooladi <76722031+fooladi-vira@users.noreply.github.com>
2024-04-05 16:08:17 +0330

    Update palindrome.py with is_my_palindrome
    added new solution

--- src/02 Identify a Palindrome/palindrome.py
+++ src/02 Identify a Palindrome/palindrome.py
@@ -4,9 +4,18 @@ def is_palindrome(phrase):
     forwards = ''.join(re.findall(r'[a-z]+', phrase.lower()))
     backwards = forwards[::-1]
     return forwards == backwards
+    
+def is_my_palindrome(s):
+    # Remove non-alphabetic characters and convert to lowercase
+    cleaned = ''.join(filter(str.isalpha, s)).lower()
+    # Check if the cleaned string is the same as its reverse
+    return cleaned == cleaned[::-1]
 
 
 # commands used in solution video for reference
 if __name__ == '__main__':
     print(is_palindrome('hello world'))  # false
     print(is_palindrome("Go hang a salami, I'm a lasagna hog."))  # true
+    print(is_my_palindrome('hello world'))  # false
+    print(is_my_palindrome("Go hang a salami, I'm a lasagna hog."))  # true
+    
